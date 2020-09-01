53fb32a12256025cc329d4f82f141749e5f5c747
Matthias Mau <matthias.mau@outlook.com>
2020-09-01 17:07:09 +0300

    updated mongodb connection url

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.static("public"));
 // const workItems = [];
 
 // new DB setup
-mongoose.connect('mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@' + process.env.DB_CLUSTER + '.zrs4w.mongodb.net/todolistDB?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true}).catch(error => console.log(error));
+mongoose.connect('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@cluster0-shard-00-00.zrs4w.mongodb.net:27017,cluster0-shard-00-01.zrs4w.mongodb.net:27017,cluster0-shard-00-02.zrs4w.mongodb.net:27017/todolistDB?ssl=true&replicaSet=atlas-12hhxg-shard-0&authSource=admin&retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true}).catch(error => console.log(error));
 
 const itemsSchema = new mongoose.Schema({
   name: String
